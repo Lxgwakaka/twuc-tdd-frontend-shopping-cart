d3e4e9457e6848c4fc75c5e90457f90b3428dcc8
Lxgwakaka <xuguang_lv@163.com>
2022-03-02 16:56:19 +0800

    [lxg]:test1

--- src/pages/shoppingCart/index.jsx
+++ src/pages/shoppingCart/index.jsx
@@ -7,6 +7,15 @@ class ShoppingCart extends React.Component {
         return (
             <div  className="wrapper">
                 <div className="title">Shopping Cart</div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th className="align-left">商品名</th>
+                            <th>单价</th>
+                            <th className="align-right">数量</th>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         );
     }
--- src/pages/shoppingCart/test/index.test.js
+++ src/pages/shoppingCart/test/index.test.js
@@ -1,12 +1,23 @@
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
 import ShoppingCart from '../index';
 
 describe('Shopping Cart', () => {
     test('should show shopping cart page', () => {
-        const { getByText } = render(<ShoppingCart/>);
+        const {getByText} = render(<ShoppingCart/>);
 
         expect(getByText('Shopping Cart')).toBeInTheDocument();
     });
+
+    test('should show empty', () => {
+        const {container} = render(<ShoppingCart/>);
+        const thElement = container.getElementsByTagName('th');
+        expect(thElement.length).toEqual(3);
+        ['商品名', '单价', '数量'].forEach((txt, index) => {
+            expect(thElement.item(index)).toHaveTextContent(txt);
+        });
+    });
+
+
 });
